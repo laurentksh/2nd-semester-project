50d2a2501ed7de955c1807b5e49bd341566860d4
Ethan <49977117+Ethan0079@users.noreply.github.com>
2020-01-30 10:43:11 +0100

    Avancement générale sur le design
    Création de la page contact changement de design etc

--- 2ndSemesterProject/Controllers/HomeController.cs
+++ 2ndSemesterProject/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace _2ndSemesterProject.Controllers
             return View();
         }
 
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
