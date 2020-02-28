a3fa58e81ccf4805da83c19384d197dcb6e01d3d
Laurent <25180858+laurentksh@users.noreply.github.com>
2020-02-29 00:27:37 +0100

    Minor changes
    -Fixed website's layout
    -Renamed MyFolders to my-cloud
    -Added CRUD-Related views for CloudController.
    -Added a temporary "AllViews" page that display all views.

---
+++ 2ndSemesterProject/Controllers/CloudController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _2ndSemesterProject.Controllers
+{
+    [Route("/my-cloud", Name = "cloud")]
+    public class CloudController : Controller
+    {
+        // GET: my-cloud
+        [Route("/")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: my-cloud/Details/5
+        [Route("/Details/{id}")]
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: my-cloud/Create
+        [Route("/Create")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // GET: my-cloud/Edit/5
+        [Route("/Edit/{id}")]
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // GET: my-cloud/Delete/5
+        [Route("/Delete/{id}")]
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+
+        // POST: my-cloud/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Route("/Create")]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // POST: my-cloud/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Route("/Edit/{id}")]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // POST: my-cloud/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Route("/Delete/{id}")]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- 2ndSemesterProject/Controllers/HomeController.cs
+++ 2ndSemesterProject/Controllers/HomeController.cs
@@ -7,16 +7,20 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using _2ndSemesterProject.Models;
 using System.Globalization;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 namespace _2ndSemesterProject.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IActionDescriptorCollectionProvider _provider;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IActionDescriptorCollectionProvider provider)
         {
             _logger = logger;
+            _provider = provider;
         }
 
         public IActionResult Index()
@@ -26,6 +30,15 @@ namespace _2ndSemesterProject.Controllers
 
         public IActionResult Privacy()
         {
+            return View();
+        }
+
+        public IActionResult AllViews()
+        {
+            var routes = _provider.ActionDescriptors.Items.ToList();
+
+            ViewBag.AllViews = routes;
+
             return View();
         }
 
@@ -47,10 +60,6 @@ namespace _2ndSemesterProject.Controllers
 
             ViewBag.CountryList = CountryList.Prepend("Select a country...");
 
-            return View();
-        }
-        public IActionResult Myfolders()
-        {
             return View();
         }
 
