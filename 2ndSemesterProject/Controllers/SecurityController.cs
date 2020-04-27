98a0bced2dc651f8a489cff8eb079a94760ae460
Ethan <marchand.ethan@bluewin.ch>
2020-04-27 12:33:46 +0200

    Formulaire

--- 2ndSemesterProject/Controllers/SecurityController.cs
+++ 2ndSemesterProject/Controllers/SecurityController.cs
@@ -21,5 +21,11 @@ namespace _2ndSemesterProject.Controllers
             return View();
 
         }
+        [Route("ResetPasswords")]
+        public IActionResult ResetPasswords()
+        {
+            return View();
+
+        }
     }  
 }
